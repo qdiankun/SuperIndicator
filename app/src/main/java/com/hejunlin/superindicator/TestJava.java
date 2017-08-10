1a3530628f0f7f85cd1ae3584288747dae0c4980
diankun.qiu <diankun.qiu@ecovacs.com>
2017-08-10 09:37:26 +0800

    ignore build file

---
+++ app/src/main/java/com/hejunlin/superindicator/TestJava.java
@@ -0,0 +1,13 @@
+package com.hejunlin.superindicator;
+
+/**
+ * Created by diankun.qiu on 2017/8/9.
+ */
+
+public class TestJava {
+
+    public static void main(String[] args) {
+        int i = (0 - 1) % 5;
+        System.out.println("(0-1)%5 = " + i);
+    }
+}
